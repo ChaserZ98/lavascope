cdf80cf63be5c3ce936eed6f0af13181b3600fb4
Feiyu Zheng <feiyuzheng98@gmail.com>
2025-02-13 18:14:26 +0800

    🦄 refactor: Store tray icon menu instance in tauri state to better manage it later

--- src-tauri/src/app/tray.rs
+++ src-tauri/src/app/tray.rs
@@ -4,22 +4,28 @@ use tauri::{
     AppHandle, Manager, Wry,
 };
 
+struct MenuState {
+    menu: Menu<Wry>,
+}
+
 pub fn create_tray(app: &AppHandle) -> Result<TrayIcon, tauri::Error> {
     let hide_or_show = MenuItem::with_id(app, "hide/show", "Hide", true, None::<&str>)?;
     let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
     let menu = Menu::with_items(app, &[&hide_or_show, &quit])?;
+    app.manage(MenuState { menu });
 
     TrayIconBuilder::with_id("tray")
         .tooltip("Vultr Firewall Watcher")
         .icon(app.default_window_icon().unwrap().clone())
-        .menu(&menu)
+        .menu(&app.state::<MenuState>().menu)
         .show_menu_on_left_click(false)
         .on_menu_event(|app, event| menu_click_handler(app, &event))
-        .on_tray_icon_event(move |tray, event| system_tray_event_handler(tray, &event, &menu))
+        .on_tray_icon_event(move |tray, event| system_tray_event_handler(tray, &event))
         .build(app)
 }
 
-fn system_tray_event_handler(tray: &TrayIcon, event: &TrayIconEvent, menu: &Menu<Wry>) {
+fn system_tray_event_handler(tray: &TrayIcon, event: &TrayIconEvent) {
+    let menu = &tray.app_handle().state::<MenuState>().menu;
     match event {
         TrayIconEvent::Click {
             button: MouseButton::Left,
@@ -50,16 +56,17 @@ fn left_click_handler(tray: &TrayIcon) {
 }
 
 fn right_click_handler(tray: &TrayIcon, menu: &Menu<Wry>) {
-    let app = tray.app_handle();
-    let menu_item_generic = menu.get("hide/show").unwrap();
-    let hide_or_show = menu_item_generic.as_menuitem().unwrap();
-    let window = app.get_webview_window("main").unwrap();
-    let new_text = if window.is_visible().unwrap_or(false) {
-        "Hide"
-    } else {
-        "Show"
+    let window = tray.app_handle().get_webview_window("main").unwrap();
+    let new_text = match window.is_visible().unwrap_or(false) {
+        true => "Hide",
+        false => "Show",
     };
-    hide_or_show.set_text(new_text).unwrap();
+    menu.get("hide/show")
+        .unwrap()
+        .as_menuitem()
+        .unwrap()
+        .set_text(new_text)
+        .unwrap();
 }
 
 fn menu_click_handler(app: &AppHandle, event: &MenuEvent) {
