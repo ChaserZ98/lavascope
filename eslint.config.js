fbb95b53cd78483acb23c664e1130ddd9e4288ce
ChaserZ98 <feiyuzheng98@gmail.com>
2025-03-12 05:06:25 +0800

    🐳 chore: Add config for @stylistic/eslint-plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import pluginJs from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import pluginLingui from "eslint-plugin-lingui";
 import pluginReact from "eslint-plugin-react";
@@ -47,4 +48,28 @@ export default [
         },
     },
     ...pluginQuery.configs["flat/recommended"],
+    stylistic.configs.customize({
+        indent: 4,
+        quotes: "double",
+        semi: true,
+        jsx: true,
+        commaDangle: "only-multiline",
+        braceStyle: "1tbs",
+        arrowParens: "always",
+    }),
+    {
+        plugins: {
+            "@stylistic": stylistic,
+        },
+        rules: {
+            "@stylistic/indent": ["error", 4, {
+                ObjectExpression: "first",
+                SwitchCase: 1,
+            }],
+            "@stylistic/operator-linebreak": ["error", "after"],
+            "@stylistic/eol-last": ["error", "always"],
+            "@stylistic/quote-props": ["off"],
+            "@stylistic/jsx-one-expression-per-line": ["off"],
+        }
+    }
 ];
