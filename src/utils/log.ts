2e7013a35a02284cf32caec3405870523f90b4a3
Feiyu Zheng <feiyuzheng98@gmail.com>
2025-03-11 22:16:25 +0800

    ✨ feat: enable log level

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,55 +1,103 @@
-import { debug, error, info, trace, warn } from "@tauri-apps/plugin-log";
+import { isTauri } from "@tauri-apps/api/core";
+import { debug, error, info, warn } from "@tauri-apps/plugin-log";
 
-type Log = {
-    error: (msg: string) => void;
-    warn: (msg: string) => void;
-    info: (msg: string) => void;
-    debug: (msg: string) => void;
-    trace: (msg: string) => void;
-};
-
-function forwardLog(level: "error" | "warn" | "info" | "debug" | "log") {
-    let tauriLog: (msg: string) => void;
-    switch (level) {
-        case "error":
-            tauriLog = error;
-            break;
-        case "warn":
-            tauriLog = warn;
-            break;
-        case "info":
-            tauriLog = info;
-            break;
-        case "debug":
-            tauriLog = debug;
-            break;
-        default:
-            tauriLog = trace;
-    }
-    return (msg: string) => {
-        console[level](msg);
-        try {
-            tauriLog(msg);
-        } catch (err) {
-            console.error(err);
-        }
-    };
+enum LogLevel {
+    TRACE = 0,
+    DEBUG = 10,
+    INFO = 20,
+    WARN = 30,
+    ERROR = 40,
+    CRITICAL = 50,
 }
 
-const logging: Log =
-    "__TAURI_INTERNALS__" in window
-        ? {
-              error: forwardLog("error"),
-              warn: forwardLog("warn"),
-              info: forwardLog("info"),
-              debug: forwardLog("debug"),
-              trace: forwardLog("log"),
-          }
-        : {
-              error: console.error,
-              warn: console.warn,
-              info: console.info,
-              debug: console.debug,
-              trace: console.log,
-          };
+type Log = {
+    trace: (msg: string) => void;
+    debug: (msg: string) => void;
+    info: (msg: string) => void;
+    warn: (msg: string) => void;
+    error: (msg: string) => void;
+};
+
+function forwardLog(
+    level: "error" | "warn" | "info" | "debug" | "log",
+    frontEndLogLevel: LogLevel = import.meta.env.PROD
+        ? LogLevel.WARN
+        : LogLevel.DEBUG,
+    tauriLogLevel: LogLevel = import.meta.env.PROD
+        ? LogLevel.INFO
+        : LogLevel.DEBUG
+) {
+    switch (level) {
+        case "debug": {
+            const frontEndLogger =
+                frontEndLogLevel > LogLevel.DEBUG ? () => {} : console.debug;
+            const tauriLogger =
+                tauriLogLevel > LogLevel.DEBUG ? () => {} : debug;
+            return (msg: string) => {
+                frontEndLogger(msg);
+                tauriLogger(msg);
+            };
+        }
+        case "info": {
+            const frontEndLogger =
+                frontEndLogLevel > LogLevel.INFO ? () => {} : console.info;
+            const tauriLogger = tauriLogLevel > LogLevel.INFO ? () => {} : info;
+            return (msg: string) => {
+                frontEndLogger(msg);
+                tauriLogger(msg);
+            };
+        }
+        case "warn": {
+            const frontEndLogger =
+                frontEndLogLevel > LogLevel.WARN ? () => {} : console.warn;
+            const tauriLogger = tauriLogLevel > LogLevel.WARN ? () => {} : warn;
+            return (msg: string) => {
+                frontEndLogger(msg);
+                tauriLogger(msg);
+            };
+        }
+        case "error": {
+            const frontEndLogger =
+                frontEndLogLevel > LogLevel.ERROR ? () => {} : console.error;
+            const tauriLogger =
+                tauriLogLevel > LogLevel.ERROR ? () => {} : error;
+            return (msg: string) => {
+                frontEndLogger(msg);
+                tauriLogger(msg);
+            };
+        }
+        case "log": {
+            const frontEndLogger =
+                frontEndLogLevel > LogLevel.INFO ? () => {} : console.log;
+            const tauriLogger = tauriLogLevel > LogLevel.INFO ? () => {} : info;
+            return (msg: string) => {
+                frontEndLogger(msg);
+                tauriLogger(msg);
+            };
+        }
+    }
+}
+
+// const tauriLogLevel = import.meta.env.PROD ? LogLevel.INFO : LogLevel.DEBUG;
+// const frontEndLogLevel = import.meta.env.PROD ? LogLevel.WARN : LogLevel.DEBUG;
+
+const tauriLogLevel = LogLevel.INFO;
+const frontEndLogLevel = LogLevel.WARN;
+
+const logging: Log = isTauri()
+    ? {
+          error: forwardLog("error", frontEndLogLevel, tauriLogLevel),
+          warn: forwardLog("warn", frontEndLogLevel, tauriLogLevel),
+          info: forwardLog("info", frontEndLogLevel, tauriLogLevel),
+          debug: forwardLog("debug", frontEndLogLevel, tauriLogLevel),
+          trace: forwardLog("log", frontEndLogLevel, tauriLogLevel),
+      }
+    : {
+          error: frontEndLogLevel > LogLevel.ERROR ? () => {} : console.error,
+          warn: frontEndLogLevel > LogLevel.WARN ? () => {} : console.warn,
+          info: frontEndLogLevel > LogLevel.INFO ? () => {} : console.info,
+          debug: frontEndLogLevel > LogLevel.DEBUG ? () => {} : console.debug,
+          trace: frontEndLogLevel > LogLevel.TRACE ? () => {} : console.trace,
+      };
+
 export default logging;
