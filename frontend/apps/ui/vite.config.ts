99b5d502521b5a322da869d5f4ecaa00a64247b7
ChaserZ98 <feiyuzheng98@gmail.com>
2025-10-09 03:25:28 +0800

    🦄 refactor: update path to backend

--- frontend/apps/ui/vite.config.ts
+++ frontend/apps/ui/vite.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
         port: 1420,
         strictPort: true,
         watch: {
-            // 3. tell vite to ignore watching `src-tauri`
-            ignored: ["**/src-tauri/**"],
+            // 3. tell vite to ignore watching `backend`
+            ignored: ["**/backend/**"],
         },
     },
 });
