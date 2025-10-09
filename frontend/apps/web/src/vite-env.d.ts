a16eca7fd17e959976c8444ea78518f4ccdb6bcc
ChaserZ98 <feiyuzheng98@gmail.com>
2025-10-10 01:54:26 +0800

    🔧 build: add build info to vite

--- frontend/apps/web/src/vite-env.d.ts
+++ frontend/apps/web/src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+declare const __BUILD_INFO__: {
+    ui_version: string;
+    cargo_package_version: string;
+    tauri_config_version: string;
+    git_commit_hash: string;
+};
