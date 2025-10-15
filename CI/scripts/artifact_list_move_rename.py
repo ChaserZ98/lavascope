1b79897f2cb39940e05931e0f8b2c6cb7cabc2ab
ChaserZ98 <feiyuzheng98@gmail.com>
2025-10-16 04:26:02 +0800

    refactor(workflows): rename python scripts to use snake_case
    - Rename `update-release-link.py` to `update_release_link.py` for consistency
    - Move inline Python code in GitHub workflows to dedicated scripts:
      - `get_commit_sha_short.py` for fetching short commit SHA
      - `artifact_list_move_rename.py` for handling artifact renaming and moving
    - Update workflow references to use the new script names

---
+++ CI/scripts/artifact_list_move_rename.py
@@ -0,0 +1,25 @@
+import json
+import os
+import shutil
+from pathlib import Path
+
+
+def artifact_list_move_rename():
+    artifactArray = json.loads(os.getenv("RELEASE_ACTIFACTS", "[]"))
+    for artifact_path in artifactArray:
+        print(artifact_path)
+
+        file_name = Path(artifact_path).name
+        file_name_parts = file_name.split("_")
+        if len(file_name_parts) > 1:
+            file_name_parts[1] = os.getenv("COMMIT_SHA_SHORT", "")
+        new_file_name: str = "_".join(file_name_parts)
+        temp_dir = Path(os.getenv("RUNNER_TEMP", ""))
+
+        target_path = temp_dir.joinpath(new_file_name)
+        print(f"Moving and renaming ${artifact_path} to ${target_path}")
+        shutil.move(artifact_path, temp_dir.joinpath(new_file_name))
+
+
+if __name__ == "__main__":
+    artifact_list_move_rename()
---
+++ CI/scripts/get_commit_sha_short.py
@@ -0,0 +1,12 @@
+import os
+
+
+def get_commit_sha_short():
+    commit_sha_short = os.popen("git rev-parse --short HEAD").read().strip()
+    print(f"commit_sha_short: ${commit_sha_short}")
+    with open(os.getenv("GITHUB_ENV"), "a") as env_file:
+        env_file.write(f"COMMIT_SHA_SHORT={commit_sha_short}")
+
+
+if __name__ == "__main__":
+    get_commit_sha_short()
